c1acd612b63365597aa62e4dbef80237fdf9e167
Nguyen Thi Tuyet Nhi <163515714+NhyYi05@users.noreply.github.com>
2025-11-27 19:54:12 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -23,10 +24,9 @@ function App() {
           </Route>
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/login" element={<LoginSignup/>}/>
-
         </Routes>
+        <Footer/>
       </BrowserRouter>
-
     </div>
   );
 }
